93960bf3d870189ed6157224bfa1ca4c3497b17a
Rexman17 <a.stone1794@gmail.com>
2018-10-13 16:50:07 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,66 +1,42 @@
-class CashRegister
-  #define the methods for the instance variables
-  attr_accessor :total, :discount, :items, :last_transaction, :quantity, :title
+require 'pry'
 
-  def initialize(discount = 0) # default val if no discount
+class CashRegister
+
+  attr_accessor :discount, :total, :items, :last_transaction # going to change, so must be able to be reset and read out
+
+  def initialize(discount = 0) # optional discount upon initialization
+    @discount = discount
     @total = 0 # initialize total at 0
-    @discount = discount # if discount is there then it equal discount
-    @items = [] # empty cart here
+    @items = []
   end
 
-  def add_item(title, price, quantity = 1)
-    @total += (price * quantity)
-
-    while quantity > 0
+  # INSTANCE METHODS => BEHAVIORS, not ATTRIBUTES like :discount, :total, etc.
+  def add_item(title, p, q=1) # accepts a title and a price AND inc. the total. Optional quantity set to 1 since u will always have at least one item.
+    @total += (p*q) # @ total is price times quantity, and increments (does not forget previous total as u add new items)
+    q.times do # want to add the item as many times to the list as its quantity so keep doing this the # of times its quantity is
       @items << title
-      quantity -= 1
     end
-
-    @last_transaction = price # price constantly gets rewritten to most recent price 
+    @last_transaction = p # the last transaction = the price of the last item added
   end
 
   def apply_discount
-    if @discount != 0 # if there is a discount to apply...
-      @total *= 0.80 # applying discount
-      "After the discount, the total comes to $#{@total.to_i}."
-    else # if there is NOT discount...
+    if @discount != 0 # if cash register initialized WITH a discount
+      @total *= 0.80
+      "After the discount, the total comes to $#{@total.to_i}." # had to do .to_i bc currently total is a float
+    else # else the cash register discount = 0 aka no discount
       "There is no discount to apply."
     end
   end
 
-  def items
+  def items # reader for the @items instance variable
     @items
   end
 
   def void_last_transaction
-    @total -= last_transaction
+    @total -= @last_transaction
   end
 
 end
 
-# def add_item(item, p, q=nil)
-#   @total += p*q
-# end
-
-# def add_item(title, p, q = 1)
-#   # accepts a title and a price and inc the @total
-#     # increment total by p
-#     @total += p * q
-#     q.times do
-#       @items << title
-#     end
-#     @last_transaction = p*q
-# end
-#
-# def apply_discount
-#   if @discount != 0
-#     @total *= 0.80
-#     "After the discount, the total comes to $#{self.total.to_i}."
-#   else
-#     "There is no discount to apply."
-#   end
-# end
-#
-# def void_last_transaction
-# 	@total -= @last_transaction
-# end
+# binding.pry
+# puts "hello"
